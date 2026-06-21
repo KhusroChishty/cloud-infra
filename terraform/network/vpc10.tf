b1fde5ad27a58eee226d14658131f5b3eff0204f
KhusroChishty <chishty.khusro1@gmail.com>
2026-06-21 12:50:40 +0530

    setup of vpc 10

---
+++ terraform/network/vpc10.tf
@@ -0,0 +1,117 @@
+provider "aws" {
+    region = "us-west-1"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags = {
+        Name = "prod-vpc"
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public_1" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-west-1a"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "us-west-1b"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_3" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "us-west-1c"
+    map_public_ip_on_launch = true
+}
+
+
+resource "aws_subnet" "private_1" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.11.0/24"
+    availability_zone = "us-west-1a"
+
+}
+
+resource "aws_subnet" "private_2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.12.0/24"
+    availability_zone = "us-west-1b"
+    
+}
+resource "aws_subnet" "private_3" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.13.0/24"
+    availability_zone = "us-west-1c"
+    
+}
+
+resource "aws_eip" "nat_eip" {
+    domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.public_1.id
+}
+
+
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
+
+resource "aws_route_table" "private_rt" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat.id
+    }
+}
+
+resource "aws_route_table_association" "public_assoc_1" {
+    subnet_id = aws_subnet.public_1.id
+    route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_assoc_2" {
+    subnet_id = aws_subnet.public_2.id
+    route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_assoc_3" {
+    subnet_id = aws_subnet.public_3.id
+    route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc_1" {
+    subnet_id = aws_subnet.private_1.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc_2" {
+    subnet_id = aws_subnet.private_2.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc_3" {
+    subnet_id = aws_subnet.private_3.id
+    route_table_id = aws_route_table.private_rt.id
+}
